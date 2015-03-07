91e21bf80e0d46d5de9bd264875132c95f189f30
janfietz <janfietz@gmail.com>
2015-03-07 20:45:36 +0100

    ws2811: add led driver

---
+++ ws2811.c
@@ -0,0 +1,171 @@
+/**
+ * @file    ws2811.c
+ * @brief   WS2811 Driver code.
+ *
+ * @addtogroup WS2811
+ * @{
+ */
+
+#include "hal.h"
+#include "ws2811.h"
+
+#if HAL_USE_WS2811 || defined(__DOXYGEN__)
+
+/*===========================================================================*/
+/* Driver local definitions.                                                 */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver exported variables.                                                */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local variables and types.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local functions.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver exported functions.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   WS2811 Driver initialization.
+ * @note    This function is implicitly invoked by @p halInit(), there is
+ *          no need to explicitly initialize the driver.
+ *
+ * @init
+ */
+void ws2811Init(void) {
+
+}
+
+/**
+ * @brief   Initializes the standard part of a @p WS2811Driver structure.
+ *
+ * @param[out] ws2811p     pointer to the @p WS2811Driver object
+ *
+ * @init
+ */
+void ws2811ObjectInit(ws2811Driver *ws2811p) {
+
+	ws2811p->state = WS2811_STOP;
+	ws2811p->config = NULL;
+}
+
+/**
+ * @brief   Configures and activates the WS2811 peripheral.
+ *
+ * @param[in] ws2811p      pointer to the @p WS2811Driver object
+ * @param[in] config    pointer to the @p WS2811Config object
+ *
+ * @api
+ */
+void ws2811Start(ws2811Driver *ws2811p, const ws2811Config *config) {
+
+	osalDbgCheck((ws2811p != NULL) && (config != NULL));
+
+	osalSysLock();
+	osalDbgAssert((ws2811p->state == WS2811_STOP) || (ws2811p->state == WS2811_READY),
+			"invalid state");
+	ws2811p->config = config;
+
+	osalSysUnlock();
+
+	// configure pwm timers -
+	// timer 2 as master, active for data transmission and inactive to disable transmission during reset period (50uS)
+	// timer 3 as slave, during active time creates a 1.25 uS signal, with duty cycle controlled by frame buffer values
+
+	ws2811p->framebuffer = chHeapAlloc(NULL, ((ws2811p->config->ledCount) * 24)+10);
+
+	int j;
+	for (j = 0; j < (ws2811p->config->ledCount) * 24; j++)
+	ws2811p->framebuffer[j] = 0;
+
+	ws2811p->dma_source = ws2811p->config->mask;
+
+	// DMA stream 2, triggered by channel3 pwm signal. if FB indicates, reset output value early to indicate "0" bit to ws2812
+	dmaStreamAllocate(ws2811p->config->dmaStreamZero, 10, NULL, NULL);
+	dmaStreamSetPeripheral(ws2811p->config->dmaStreamZero, ws2811p->config->dmaStreamZero_periphal);
+	dmaStreamSetMemory0(ws2811p->config->dmaStreamZero, ws2811p->framebuffer);
+	dmaStreamSetTransactionSize(ws2811p->config->dmaStreamZero, (ws2811p->config->ledCount) * 24);
+	dmaStreamSetMode(
+			ws2811p->config->dmaStreamZero,
+			STM32_DMA_CR_DIR_M2P | STM32_DMA_CR_MINC | STM32_DMA_CR_PSIZE_BYTE
+			| STM32_DMA_CR_MSIZE_BYTE | STM32_DMA_CR_CIRC | STM32_DMA_CR_PL(2));
+	// DMA stream 3, triggered by pwm update event. output high at beginning of signal
+	dmaStreamAllocate(ws2811p->config->dmaStreamReset, 10, NULL, NULL);
+	dmaStreamSetPeripheral(ws2811p->config->dmaStreamReset, ws2811p->config->dmaStreamReset_periphal);
+	dmaStreamSetMemory0(ws2811p->config->dmaStreamReset, &ws2811p->dma_source);
+	dmaStreamSetTransactionSize(ws2811p->config->dmaStreamReset, 1);
+	dmaStreamSetMode(
+			ws2811p->config->dmaStreamReset, STM32_DMA_CR_TEIE |
+			STM32_DMA_CR_DIR_M2P | STM32_DMA_CR_PSIZE_BYTE | STM32_DMA_CR_MSIZE_BYTE
+			| STM32_DMA_CR_CIRC | STM32_DMA_CR_PL(3));
+  
+	// DMA stream 6, triggered by channel1 update event. reset output value late to indicate "1" bit to ws2812.
+	// always triggers but no affect if dma stream 2 already change output value to 0
+	dmaStreamAllocate(ws2811p->config->dmaStreamOne, 10, NULL, NULL);
+	dmaStreamSetPeripheral(ws2811p->config->dmaStreamOne, ws2811p->config->dmaStreamOne_periphal);
+	dmaStreamSetMemory0(ws2811p->config->dmaStreamOne, &ws2811p->dma_source);
+	dmaStreamSetTransactionSize(ws2811p->config->dmaStreamOne, 1);
+	dmaStreamSetMode(
+			ws2811p->config->dmaStreamOne,
+			STM32_DMA_CR_DIR_M2P | STM32_DMA_CR_PSIZE_BYTE | STM32_DMA_CR_MSIZE_BYTE
+			| STM32_DMA_CR_CIRC | STM32_DMA_CR_PL(3));
+
+	pwmStart(ws2811p->config->pwmMaster, &ws2811p->config->pwmMasterConfig);
+	pwmStart(ws2811p->config->pwmSlave, &ws2811p->config->pwmSlaveConfig);
+	// set pwm3 as slave, triggerd by pwm2 oc1 event. disables pwmd2 for synchronization.
+	ws2811p->config->pwmSlave->tim->SMCR |= TIM_SMCR_SMS_0 | TIM_SMCR_SMS_2 | TIM_SMCR_TS_0;
+	ws2811p->config->pwmMaster->tim->CR1 &= ~TIM_CR1_CEN;
+	// set pwm values.
+	// 28 (duty in ticks) / 90 (period in ticks) * 1.25uS (period in S) = 0.39 uS
+	pwmEnableChannel(ws2811p->config->pwmSlave, 2, 28);
+	// 58 (duty in ticks) / 90 (period in ticks) * 1.25uS (period in S) = 0.806 uS
+	pwmEnableChannel(ws2811p->config->pwmSlave, 0, 58);
+	// active during transfer of 90 cycles * ws2811p->config->ledCount * 24 bytes * 1/90 multiplier
+	pwmEnableChannel(ws2811p->config->pwmMaster, 0, 90 * ws2811p->config->ledCount * 24 / 90);
+	// stop and reset counters for synchronization
+	ws2811p->config->pwmMaster->tim->CNT = 0;
+	// Slave (TIM3) needs to "update" immediately after master (TIM2) start in order to start in sync.
+	// this initial sync is crucial for the stability of the run
+	ws2811p->config->pwmSlave->tim->CNT = 89;
+	ws2811p->config->pwmSlave->tim->DIER |= TIM_DIER_CC3DE | TIM_DIER_CC1DE | TIM_DIER_UDE;
+	dmaStreamEnable(ws2811p->config->dmaStreamReset);
+	dmaStreamEnable(ws2811p->config->dmaStreamOne);
+	dmaStreamEnable(ws2811p->config->dmaStreamZero);
+	// all systems go! both timers and all channels are configured to resonate
+	// in complete sync without any need for CPU cycles (only DMA and timers)
+	// start pwm2 for system to start resonating
+	ws2811p->config->pwmMaster->tim->CR1 |= TIM_CR1_CEN;
+
+	osalSysLock();
+	ws2811p->state = WS2811_READY;
+	osalSysUnlock();
+}
+
+/**
+ * @brief   Deactivates the WS2811 peripheral.
+ *
+ * @param[in] ws2811p      pointer to the @p WS2811Driver object
+ *
+ * @api
+ */
+void ws2811Stop(ws2811Driver *ws2811p) {
+
+	osalDbgCheck(ws2811p != NULL);
+
+	osalSysLock();
+	osalDbgAssert((ws2811p->state == WS2811_STOP) || (ws2811p->state == WS2811_READY),
+			"invalid state");
+
+	ws2811p->state = WS2811_STOP;
+	osalSysUnlock();
+}
+
+#endif /* HAL_USE_WS2811 */
+
+/** @} */
---
+++ ws2811.h
@@ -0,0 +1,108 @@
+
+/**
+ * @file    ws2811.h
+ * @brief   WS2811 Driver macros and structures.
+ *
+ * @addtogroup WS2811
+ * @{
+ */
+
+#ifndef _WS2811_H_
+#define _WS2811_H_
+
+#if HAL_USE_WS2811 || defined(__DOXYGEN__)
+
+/*===========================================================================*/
+/* Driver constants.                                                         */
+/*===========================================================================*/
+
+
+/*===========================================================================*/
+/* Driver pre-compile time settings.                                         */
+/*===========================================================================*/
+
+
+/*===========================================================================*/
+/* Derived constants and error checks.                                       */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver data structures and types.                                         */
+/*===========================================================================*/
+
+/**
+ * @brief   Driver state machine possible states.
+ */
+typedef enum {
+  WS2811_UNINIT = 0,                   /**< Not initialized.                   */
+  WS2811_STOP = 1,                     /**< Stopped.                           */
+  WS2811_READY = 2,                    /**< Ready.                             */
+  WS2811_ACTIVE = 3,                   /**< Active.                            */
+} ws2811state_t;
+/**
+ * @brief   Type of a structure representing an ws2811Driver driver.
+ */
+typedef struct ws2811Driver ws2811Driver;
+
+
+/**
+ * @brief   Driver configuration structure.
+ * @note    It could be empty on some architectures.
+ */
+typedef struct {
+  uint16_t ledCount;
+  uint32_t mask;
+  PWMConfig pwmMasterConfig;
+  PWMDriver *pwmMaster;
+  PWMConfig pwmSlaveConfig;
+  PWMDriver *pwmSlave;
+  stm32_dma_stream_t *dmaStreamZero;
+  void* dmaStreamZero_periphal;
+  stm32_dma_stream_t *dmaStreamOne;
+  void* dmaStreamOne_periphal;
+  stm32_dma_stream_t *dmaStreamReset;
+  void* dmaStreamReset_periphal;
+} ws2811Config;
+
+
+/**
+ * @brief   Structure representing an ws2811 driver.
+ */
+struct ws2811Driver {
+  /**
+   * @brief   Driver state.
+   */
+	ws2811state_t                state;
+  /**
+   * @brief   Current configuration data.
+   */
+  const ws2811Config           *config;
+  /* End of the mandatory fields.*/
+
+  uint8_t dma_source;
+  uint8_t *framebuffer;
+};
+/*===========================================================================*/
+/* Driver macros.                                                            */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+  void ws2811Init(void);
+  void ws2811ObjectInit(ws2811Driver *ws2811p);
+  void ws2811Start(ws2811Driver *ws2811p, const ws2811Config *config);
+  void ws2811Stop(ws2811Driver *ws2811p);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HAL_USE_ws2811 */
+
+#endif /* _WS2811_H_ */
+
+/** @} */
